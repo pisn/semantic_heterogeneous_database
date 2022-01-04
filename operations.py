a73205d984384afc2f5b855824574db5c97e7734
Pedro Ivo <pedro.isn1@gmail.com>
2022-01-04 00:34:39 -0300

    comecando evolução dos registros

--- operations.py
+++ operations.py
@@ -1,3 +1,4 @@
+from bson.objectid import ObjectId
 from pymongo import MongoClient
 import json
 
@@ -7,17 +8,86 @@ class InterscityCollection:
         self.db = self.client[databaseName]
         self.collection = self.db[collectionName]
         self.collection_columns = self.db[collectionName+'_columns']
+        self.collection_versions = self.db[collectionName + '_versions']
+        self.current_version = self.collection_versions.find_one({"current_version":1})
+        
+        #initializing first version
+        if(self.current_version == None):
+            self.current_version = 0
+           
+            first_version = {
+                "current_version":1,
+                "previous_version":None, 
+                "previous_operation":None,
+                "version_number":0, 
+                "next_version":None,
+                "next_operation":None
+            }
+            self.collection_versions.insert_one(first_version)                       
+        else:
+            self.current_version = self.current_version['version_number']
 
     def insert_one(self, jsonString):
-        o = json.loads(jsonString)
+        o = json.loads(jsonString)        
 
-        insertedDocument = self.collection.insert_one(o)
+        insertedDocument = self.collection.insert_one({("version_" + str(self.current_version)): o})
 
         for field in o:
             if not field.startswith('_'):
                 self.collection_columns.update_one({'name': field}, {'$set' : {'name' : field}, '$push' : {'documents' : insertedDocument.inserted_id}}, upsert=True)
 
+    def execute_translation(self, fieldName, oldValue, newValue, eagerlyTranslate):        
+        #bater nas colunas e gerar novas versoes se eagerly
+        #salvar traducao em algum lugar para ser consultada de qualquer maneira
+        new_version = {
+            "current_version":1,
+            "previous_version":self.current_version,
+            "previous_operation": {
+                "type": "translation",
+                "field":fieldName,
+                "from":newValue,
+                "to":oldValue
+            },
+            "next_version":None,
+            "next_operation":None,
+            "version_number":self.current_version + 1
+        }
+
+        next_operation = {
+            "type": "translation",
+            "field": fieldName,
+            "from":oldValue,
+            "to":newValue
+        }
+        
+        res = self.collection_versions.update_one({'current_version': 1}, {'$set' : {'current_version' : 0, 'next_operation': next_operation}})
+        
+        if(res.matched_count != 1):
+            print("Current version not matched")
+
+        self.collection_versions.insert_one(new_version)        
+        self.current_version = self.current_version + 1
+
+        if(eagerlyTranslate):
+            documentsContainingColumn = self.collection_columns.find_one({"name":fieldName})['documents']
+
+            for documentId in documentsContainingColumn:
+                document = self.collection.find_one(ObjectId(documentId))
+                document = document['version_' + str(self.current_version-1)] #Estou assumindo que existe a versao anterior. Mas pode ter havido alterações lazy antes. Preciso percorrer de alguma forma
+
+                if(document[fieldName] == oldValue):
+                    document[fieldName] = newValue
+                
+                #put a new version even if has no changes? Should I do this for every record in the database? (hint:no)
+                self.collection.update_one({'_id' : documentId}, {'$set' : {'version_' + str(self.current_version) : document}})
+                
+
+                
+        
+        
 
 myCollection = InterscityCollection('interscity', 'collectionTest')
 myCollection.insert_one('{"coluna1": "seila"}')
-        
+myCollection.insert_one('{"coluna2": "seila"}')
+myCollection.insert_one('{"coluna1": "seila2"}')
+myCollection.execute_translation("coluna1","seila","seila3", True)        
