b7659b63b59d281f9270548514d92d7901626b26
Pedro Ivo <pedro.isn1@gmail.com>
2022-01-14 00:40:39 -0300

    querying one field in last version working

--- operations.py
+++ operations.py
@@ -1,3 +1,4 @@
+from platform import version
 from bson.objectid import ObjectId
 from pymongo import MongoClient
 import json
@@ -7,6 +8,7 @@ class InterscityCollection:
         self.client = MongoClient(host='localhost')
         self.db = self.client[databaseName]
         self.collection = self.db[collectionName]
+        self.collection_processed = self.db[collectionName+'_processed']
         self.collection_columns = self.db[collectionName+'_columns']
         self.collection_versions = self.db[collectionName + '_versions']
         self.current_version = self.collection_versions.find_one({"current_version":1})
@@ -29,15 +31,24 @@ class InterscityCollection:
 
     def insert_one(self, jsonString):
         o = json.loads(jsonString)        
+        o['_first_processed_version'] = self.current_version
+        o['_last_processed_version']=self.current_version
+        o['_original_version']=self.current_version       
 
-        insertedDocument = self.collection.insert_one({("version_" + str(self.current_version)): o, 'first_version': self.current_version, 'last_version': self.current_version, 'original_version' : self.current_version})        
+        insertedDocument = self.collection.insert_one(o)        
+
+        p = json.loads(jsonString)
+        p['_version_number'] = self.current_version
+        p['_original_id'] = insertedDocument.inserted_id
+
+        self.collection_processed.insert_one(p)
 
         for field in o:
             if not field.startswith('_'):
-                updateResult = self.collection_columns.update_one({'name': field}, {'$set' : {'name' : field}, '$push' : {'documents' : insertedDocument.inserted_id}}, upsert=True)
+                updateResult = self.collection_columns.update_one({'field_name': field}, {'$set' : {'field_name' : field}, '$push' : {'documents' : insertedDocument.inserted_id}}, upsert=True)
                 
                 if(updateResult.upserted_id != None):
-                    self.collection_columns.update_one({'_id':updateResult.upserted_id},{'$set' : {'first_version' : self.current_version ,'last_version': self.current_version}})
+                    self.collection_columns.update_one({'_id':updateResult.upserted_id},{'$set' : {'first_edit_version' : self.current_version ,'last_edit_version': self.current_version}})
 
     def execute_translation(self, fieldName, oldValue, newValue, eagerlyTranslate):                
         new_version = {
@@ -61,7 +72,7 @@ class InterscityCollection:
             "to":newValue
         }
         
-        res = self.collection_versions.update_one({'current_version': 1}, {'$set' : {'current_version' : 0, 'next_operation': next_operation}})
+        res = self.collection_versions.update_one({'current_version': 1}, {'$set' : {'current_version' : 0, 'next_operation': next_operation, 'next_version':self.current_version + 1}})
         
         if(res.matched_count != 1):
             print("Current version not matched")
@@ -69,20 +80,22 @@ class InterscityCollection:
         self.collection_versions.insert_one(new_version)        
         self.current_version = self.current_version + 1
 
-        self.collection_columns.update_one({'name':fieldName}, {'$set' : {'last_version' : self.current_version}})
+        self.collection_columns.update_one({'field_name':fieldName}, {'$set' : {'last_edit_version' : self.current_version}})
 
         if(eagerlyTranslate):
             for document in self.collection.find():                
                 self.evolute(document, self.current_version)
 
                 
-    def evolute(self, document, targetVersion):
-        lastVersion = int(document['last_version'])
-        firstVersion = int(document['first_version'])
+    def evolute(self, rawDocument, targetVersion):
+        lastVersion = int(rawDocument['_last_processed_version'])
+        firstVersion = int(rawDocument['_first_processed_version'])        
 
         if targetVersion > lastVersion:
-            while lastVersion < targetVersion:
-                lastVersionDocument = document['version_' + str(lastVersion)]
+            while lastVersion < targetVersion:                
+                
+                lastVersionDocument = self.collection_processed.find_one({'_original_id' : rawDocument['_id'], '_version_number': lastVersion})                
+                lastVersionDocument.pop('_id')
 
                 versionRegister = self.collection_versions.find_one({'version_number' : lastVersion})
 
@@ -102,11 +115,18 @@ class InterscityCollection:
                 else:
                     raise 'Unrecognized evolution type:' + evolutionOperation['type']        
                 
-                lastVersion = lastVersion + 1
-                self.collection.update_one({'_id' : document['_id']}, {'$set': {'version_' + str(lastVersion) : lastVersionDocument, 'last_version':lastVersion}})                
+                if(versionRegister['next_version'] != None):
+                    lastVersion = int(versionRegister['next_version'])
+                else: #Chegou a ultima versao disponivel, aumentar um ponto
+                    lastVersion = self.current_version
+                
+                lastVersionDocument['_version_number'] = lastVersion
+                rawDocument['_last_processed_version'] = lastVersion
+
+                self.collection_processed.insert_one(lastVersionDocument)
         else:
             while firstVersion > targetVersion:
-                firstVersionDocument = document['version_' + str(firstVersion)]
+                firstVersionDocument = self.collection_processed.find_one({'_original_id' : rawDocument['_id'], '_version_number': firstVersion})                
 
                 versionRegister = self.collection_versions.find_one({'version_number' : firstVersion})
 
@@ -126,8 +146,13 @@ class InterscityCollection:
                 else:
                     raise 'Unrecognized evolution type:' + evolutionOperation['type']        
                 
-                firstVersion = firstVersion - 1
-                self.collection.update_one({'_id' : document['_id']}, {'$set': {'version_' + str(firstVersion) : firstVersionDocument, 'first_version':firstVersion}})                
+                firstVersion = int(versionRegister['previous_version'])
+                firstVersionDocument['_version_number'] = firstVersion
+                rawDocument['_first_processed_version'] = firstVersion
+
+                self.collection_processed.insert_one(firstVersionDocument)
+
+        self.collection.update_one({'_id':rawDocument['_id']}, {'$set': {'_first_processed_version': rawDocument['_first_processed_version'], '_last_processed_version': rawDocument['_last_processed_version']}})
     
     def query(self, query, version_number = None):
         #Eu preciso depois permitir que seja o tempo da versao, nao o numero
@@ -135,11 +160,57 @@ class InterscityCollection:
 
         if(version_number == None):
             version_number = self.current_version   
+
+        max_version_number = version_number
+        min_version_number = version_number
+        
+        ##obtaining version to be queried
+        for field in query.keys():
+            fieldRegister = self.collection_columns.find_one({'field_name':field})
+
+            if fieldRegister == None:
+                raise 'Field not found in collection: ' + field
             
+            lastFieldVersion = int(fieldRegister['last_edit_version'])
+            firstFieldVersion = int(fieldRegister['first_edit_version'])
+
+            if version_number > lastFieldVersion and lastFieldVersion > max_version_number:
+                max_version_number = lastFieldVersion
+
+            elif version_number < firstFieldVersion and firstFieldVersion < min_version_number:
+                min_version_number = firstFieldVersion
+
+        ###Vou assumir por enquanto que estou sempre consultando a ultima versao, e que portanto sempre vou evoluir. Mas pensar no caso de que seja necessário um retrocesso
+        version_number = max_version_number
+
+        ###Obtaining records which have not been translated yet to the target version and translate them
+        to_translate_up = self.collection.find({'_last_processed_version' : {'$lt' : version_number}})
+        to_translate_down = self.collection.find({'_first_processed_version' : {'$gt' : version_number}})
+
+        for record in to_translate_up:
+            self.evolute(record, version_number)
+        
+        for record in to_translate_down:
+            self.evolute(record, version_number)
+
+        query['_version_number'] = version_number ##Retornando registros traduzidos. 
+        return self.collection_processed.find(query)
+
+
+        
+          
         
 
 myCollection = InterscityCollection('interscity', 'collectionTest')
-myCollection.insert_one('{"coluna1": "seila"}')
-myCollection.insert_one('{"coluna2": "seila"}')
-myCollection.insert_one('{"coluna1": "seila2"}')
-myCollection.execute_translation("coluna1","seila","seila3", True)        
+myCollection.insert_one('{"pais": "Brasil", "cidade":"Vila Rica"}')
+myCollection.insert_one('{"pais": "Brasil", "cidade":"Cuiabá"}')
+myCollection.insert_one('{"pais": "Brasil", "cidade":"Rio de Janeiro"}')
+myCollection.execute_translation("cidade","Vila Rica","Ouro Preto", False)        
+myCollection.insert_one('{"pais": "Brasil", "cidade":"São Paulo"}')
+testeQuery = myCollection.query({'cidade' : 'Ouro Preto'})
+
+for record in testeQuery:
+    print(record['_id'])
+
+
+
