47f3aa0093def1f38d1e51f0a6d1c568cfb37b05
Pedro Ivo <pedro.isn1@gmail.com>
2023-10-22 01:35:48 -0300

    adicionando mode nas simulações

--- simulations_writer.py
+++ simulations_writer.py
@@ -9,13 +9,14 @@ evolution_fields=[2]
 operations = range(100,1100,100)
 domain = [20,40]
 update_percent=[0,0.05,0.95,1]
+mode=['preprocess','rewrite']
 
-arguments = itertools.product(*[records,versions,fields,evolution_fields,operations,domain,update_percent])
+arguments = itertools.product(*[records,versions,fields,evolution_fields,operations,domain,update_percent,mode])
 
 out = list()
 for arg_combination in arguments:
-    output = f'{arg_combination[0]}records_{arg_combination[1]}versions_{arg_combination[2]}fields_{arg_combination[3]}evfields_{arg_combination[4]}ops_{arg_combination[5]}domain_{arg_combination[6]*100}update.csv'
-    out.append(f'python3 simulations.py --records={arg_combination[0]} --versions={arg_combination[1]} --fields={arg_combination[2]} --domain={arg_combination[5]} --repetitions={repetitions} --operations={arg_combination[4]} --destination="{output}" --method={method} --update_percent={arg_combination[6]} --evolution_fields={arg_combination[3]}; ')
+    output = f'{arg_combination[0]}records_{arg_combination[1]}versions_{arg_combination[2]}fields_{arg_combination[3]}evfields_{arg_combination[4]}ops_{arg_combination[5]}domain_{arg_combination[6]*100}update_{arg_combination[7]}.csv'
+    out.append(f'python3 simulations.py --records={arg_combination[0]} --versions={arg_combination[1]} --fields={arg_combination[2]} --domain={arg_combination[5]} --repetitions={repetitions} --operations={arg_combination[4]} --destination="{output}" --method={method} --update_percent={arg_combination[6]} --evolution_fields={arg_combination[3]} --mode={arg_combination[7]}; ')
 
 with open('simulations_batch.sh','w') as file:
     file.writelines(out)
