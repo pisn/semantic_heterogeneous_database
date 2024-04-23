a7a8737c3741898f6808fbe2799e40cf12084d35
Pedro Ivo <pedro.isn1@gmail.com>
2024-04-23 01:03:55 -0300

    Adicionando testes automáticos SUS

--- semantic_heterogeneous_database/tests/test_base.py
+++ semantic_heterogeneous_database/tests/test_base.py
@@ -8,7 +8,16 @@ class TestBase(unittest.TestCase):
         self.BasicCollection.collection.client.drop_database('test_database') ##This ensures to drop the test database if already existed
         self.BasicCollection = BasicCollection('test_database','test_collection','localhost')       
 
+        self.SusCollection = BasicCollection('sus_test','sus_test','localhost')       
+        self.SusCollection.collection.client.drop_database('sus_test') ##This ensures to drop the test database if already existed
+        self.SusCollection = BasicCollection('sus_test','sus_test','localhost')   
+
+        sus_folder = '/home/pedro/Documents/USP/Mestrado/Pesquisa/mortalidade_ano'
+        self.SusCollection.insert_many_by_csv(sus_folder + '/' + 'mortalidade_9_processed_1995.csv', 'RefDate')
+        self.SusCollection.insert_many_by_csv(sus_folder + '/' + 'mortalidade_10_processed_1996.csv', 'RefDate')
+
     def tearDown(self):
         self.BasicCollection.collection.client.drop_database('test_database')
+        self.SusCollection.collection.client.drop_database('sus_test')
     
     
---
+++ semantic_heterogeneous_database/tests/test_translation_sus.py
@@ -0,0 +1,23 @@
+from .test_base import TestBase
+from datetime import datetime
+
+class TranslationSusTest(TestBase):
+
+    def test_translation_sus(self):       
+        collection = self.SusCollection        
+        collection.execute_operation('translation',datetime(1996,1,1), {'fieldName':'cid', 'oldValue':'191 Marasmo nutricional', 'newValue':'056 Desnutrição'})        
+        
+        query = self.SusCollection.find_many({'UF':'SP','municipio':'355030 SAO PAULO','cid':'191 Marasmo nutricional','ano':1995})        
+        self.assertEqual(query[0]['ocorrencias'],14,"Os 14 óbitos por Marasmo Nutricional em 1995 deveriam continuar aparecendo (query original)")
+
+        query = self.SusCollection.find_many({'UF':'SP','municipio':'355030 SAO PAULO','cid':'056 Desnutrição','ano':1995})
+        self.assertEqual(query[0]['ocorrencias'],14,"Os 14 óbitos por Marasmo Nutricional em 1995 deveriam continuar aparecendo (query com nome novo)")
+
+        query = self.SusCollection.find_many({'UF':'SP','municipio':'355030 SAO PAULO','cid':'056 Desnutrição','ano':1996})        
+        self.assertEqual(query[0]['ocorrencias'],314,"Os 314 óbitos por Desnutrição em 1996 deveriam continuar aparecendo (query original)")
+
+        query = self.SusCollection.find_many({'UF':'SP','municipio':'355030 SAO PAULO','cid':'191 Marasmo nutricional','ano':1996})
+        self.assertEqual(query[0]['ocorrencias'],314,"Os 314 óbitos por Desnutrição em 1995 deveriam aparecer (query com nome antigo)")
+        
+
+        
